ab1eef170990b87fae9581a055d3fad52d76c8ab
Mary <marygeorge.ed@gmail.com>
2025-03-04 11:23:04 +0000

    Set up a web application

---
+++ demo/Person.cs
@@ -0,0 +1,13 @@
+public class Person{
+    string name{
+        get{
+            return name;
+        }
+        set(string name){
+            
+        }
+    }
+    int age;
+
+   
+}
--- demo/Program.cs
+++ demo/Program.cs
@@ -1,12 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double totalBudget=500;
-double grocerySpent=100;
-double entertainmentSpent=500;
-double totalSpent=grocerySpent+entertainmentSpent;
-if(totalSpent>totalBudget){
-    Console.WriteLine("You are over budget");
-}
-else{
-    Console.WriteLine("You are within budget");
-}
+
 
